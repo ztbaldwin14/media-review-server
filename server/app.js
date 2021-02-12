992ce001f5b839c317cbaa9bda26cc4240562c85
apple <apple@apples-MacBook-Air.local>
2021-02-11 19:33:33 -0500

    first commit

---
+++ server/app.js
@@ -0,0 +1,15 @@
+let express = require('express');
+const app = express();
+const sequelize = require("./db");
+let movies = require("./controllers/moviescontroller");
+let user = require("./controllers/usercontroller");
+
+sequelize.sync();
+
+app.use("/movies", movies);
+
+app.use("/user", user);
+
+app.listen(3000, function(){
+    console.log('App is listening on port 3000');
+});
---
+++ server/controllers/usercontroller.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+const User = require("../db").import("../models/user.js");
+
+module.exports = router;
